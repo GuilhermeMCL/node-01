5033d6d43e2c0f88e839d34934cb738773805403
GuilhermeMCL <guilhermemcl1503@gmail.com>
2024-11-13 08:15:42 -0300

    Method POST e GET

--- src/server.js
+++ src/server.js
@@ -1,9 +1,23 @@
 import http from "node:http"
 
+//GET => Buscar um recurso do back end
+//POST => Criar um novo recurso no back end
+//PUT => Atualizar um recurso no back end
+//PATCH => Atualizar uma Informação especifica de um recurso no back end
+//DELETE => Deletar um recurso no back end
 
-//req => requisição // res => resposta.
 const server = http.createServer((req, res) => {
-return res.end('hello world')
+    const { method, url } = req;
+
+    if(method == 'GET' && url == '/users'){
+        return res.end('Listagem de usuarios')
+    } //early return
+
+    if(method == 'POST' && url== 'users'){
+        return res.end('Criando um novo usuario')
+    }
+
+    return res.end('hello world')
 })
 
 server.listen(3333)
