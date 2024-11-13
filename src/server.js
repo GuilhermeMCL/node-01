9f4df3e132cbf9df7523c3c908edb756f636ba1b
GuilhermeMCL <guilhermemcl1503@gmail.com>
2024-11-13 15:27:58 -0300

    separando em pastas

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from "node:http"
+import { json } from "./middlewares/json.js";
 
 //GET => Buscar um recurso do back end
 //POST => Criar um novo recurso no back end
@@ -12,20 +13,7 @@ const users = []
 const server = http.createServer(async(req, res) => {
     const { method, url } = req;
 
-    const buffers = []
-
-    for await (const chunk of req) {
-    buffers.push(chunk)
-    }
-
-    try {
-         req.body = JSON.parse(Buffer.concat(buffers).toString())
-    } catch {
-        req.body = null
-
-    }
-
-
+    await json(req,res)
 
     if(method == 'GET' && url == '/users'){
         return res
