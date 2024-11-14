ed082314701ab235698c0911b71a7b41b86123a5
GuilhermeMCL <guilhermemcl1503@gmail.com>
2024-11-14 12:54:59 -0300

    Criando controlador de Rotas

--- src/database.js
+++ src/database.js
@@ -20,15 +20,15 @@ export class Database {
     }
 
     select(table) {
-        const data = this.database[table] ?? []
+        const data = this.#database[table] ?? []
         return data
     }
 
     insert(table, data) {
-        if (Array.isArray(this, database[table])) {
-            this.database[table].push(data)
+        if (Array.isArray(this.#database[table])) {
+            this.#database[table].push(data)
         } else {
-            this.database[table] = [data]
+            this.#database[table] = [data]
         }
     }
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import http from "node:http"
-import { randomUUID } from "node:crypto";
 import { json } from "./middlewares/json.js";
-import { Database } from "./database.js";
+import { routes } from "./routes.js";
+
 
 //GET => Buscar um recurso do back end
 //POST => Criar um novo recurso no back end
@@ -9,34 +9,18 @@ import { Database } from "./database.js";
 //PATCH => Atualizar uma Informação especifica de um recurso no back end
 //DELETE => Deletar um recurso no back end
 
-const database = new Database()
+
 
 
 const server = http.createServer(async(req, res) => {
     const { method, url } = req;
 
     await json(req,res)
-
-    if(method == 'GET' && url == '/users'){
-        const users =  database.select('users')
-        return res
-            .end(JSON.stringify(users))
-
-    } 
-
-    if(method == 'POST' && url== '/users'){
-        const {name , email} = req.body
-
-        const user = {
-            id: randomUUID(),
-            name,
-            email,
-
-
-        }
-        database.insert ('users', user)
-
-        return res.writeHead(201).end()
+    const route = routes.find (route => { 
+        return route.method == method && route.path == url 
+    })
+    if (route){
+    return route.handler(req, res)
     }
 
     return res.writeHead(404).end()
